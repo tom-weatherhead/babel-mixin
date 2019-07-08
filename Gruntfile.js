a5b0b29967aaaa8ec3c78d1e831c8d46267b3f77
Tom Weatherhead <tomw@2hrd4u.org>
2019-07-08 12:53:57 -0600

    0.0.2

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
-// babel-mixin/Gruntfile.js
+// tom-weatherhead/babel-mixin/Gruntfile.js
 
 'use strict';
 
-// module.exports = require('thaw-config').grunt.eslint;
-module.exports = require('thaw-config').grunt.eslint_webpack_concat_nodeunit({});
+module.exports = require('thaw-config').grunt({});
